0517779c1f2ae0564255436735564e96203403a3
Iam Slaughter <iamslaughter@Iams-MacBook-Pro.local>
2017-11-16 18:33:11 -0500

    user stuff

--- server/api/subCategories.js
+++ server/api/subCategories.js
@@ -1,13 +1,9 @@
 const router = require('express').Router()
-const { SubCategory } = require('../db/models')
+const { SubCategory, Category } = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  SubCategory.findAll({
-    // explicitly select only the id and email fields - even though
-    // users' passwords are encrypted, it won't help if we just
-    // send everything to anyone who asks!
-  })
+  SubCategory.findAll({include: [Category]})
     .then(subCategories => res.json(subCategories))
     .catch(next)
 })
