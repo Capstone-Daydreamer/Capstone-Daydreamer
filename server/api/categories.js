61e7a3895e6b6687ceea08e952444b1c33d36513
ellenormerod <ellen.ormerod17@gmail.com>
2017-11-16 17:38:46 -0600

    Update categories.js

--- server/api/categories.js
+++ server/api/categories.js
@@ -3,11 +3,7 @@ const { Category } = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  Category.findAll({
-    // explicitly select only the id and email fields - even though
-    // users' passwords are encrypted, it won't help if we just
-    // send everything to anyone who asks!
-  })
+  Category.findAll()
     .then(categories => res.json(categories))
     .catch(next)
 })
