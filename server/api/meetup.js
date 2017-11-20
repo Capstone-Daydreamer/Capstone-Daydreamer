f2603490cb9486cf985f48075f0219856ea9c2a3
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-11-20 14:18:47 -0600

    chore: Make request to category api for meetup

--- server/api/meetup.js
+++ server/api/meetup.js
@@ -4,17 +4,25 @@ const request = require('request');
 
 module.exports = router
 
-request.json = true;
 router.get('/', (req, res, next) => {
-  const options = {
-    api_key: process.env.MEETUP_API_KEY,
-  };
-
-  request('http://api.meetup.com/find/topic_categories', (error, response, body) => {
+  request({
+    url: 'http://api.meetup.com/find/upcoming_events', 
+    json: true,
+    lat: 41.8781,
+    lon: 87.6298,
+    sign: true,
+    key: process.env.MEETUP_API_KEY
+  }, (error, response, body) => {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-    console.log('request:', request);
+    res.json(body)
+  });
+})
+
+router.get('/categories', (req, res, next) => {
+  request({url: 'http://api.meetup.com/find/topic_categories', json: true}, (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     res.json(body)
   });
 })
