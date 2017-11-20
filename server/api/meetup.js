ebff2c5971415dd769858a617d9d46ec44711644
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-11-20 14:34:24 -0600

    WIP: Switching branches

--- server/api/meetup.js
+++ server/api/meetup.js
@@ -6,12 +6,11 @@ module.exports = router
 
 router.get('/', (req, res, next) => {
   request({
-    url: 'http://api.meetup.com/find/upcoming_events', 
+    url: 'http://api.meetup.com/find/upcoming_events',
+    key: process.env.MEETUP_API_KEY,
     json: true,
     lat: 41.8781,
     lon: 87.6298,
-    sign: true,
-    key: process.env.MEETUP_API_KEY
   }, (error, response, body) => {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
