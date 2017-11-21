f42b33513df61f6bf99627dc4ad2025ac69c99ce
Benjamin Odisho <Semiroundpizza8@gmail.com>
2017-11-21 14:22:16 -0600

    WIP: Jumping to pair program with the rest of the wonderful group

--- server/api/eventful.js
+++ server/api/eventful.js
@@ -1,17 +1,32 @@
 const router = require('express').Router()
 const request = require('request');
+const axios = require('axios');
 
-module.exports = router
+module.exports = router;
 
+// QUESTION: Is there an easy way to convert XML to JSON, or should I create a function for that?
 router.get('/', (req, res, next) => {
-  request({
-    url: 'http://api.eventful.com/rest/events/search',
-    json: true,
-    api_key: 'RnfqpfBxDnsDB5SG',
-    location: "CHICAGO",
-  }, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    res.json(body)
-  });
+  var options = {
+    location: 'Chicago'
+  }
+  axios.get('http://api.eventful.com/rest/events/search?' +
+    'app_key=' + process.env.EVENTFUL_API_KEY +
+    '&location=' + options.location)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+})
+
+router.get('/categories', (req, res, next) => {
+  axios.get('http://api.eventful.com/rest/categories/list?' +
+    'app_key=' + process.env.EVENTFUL_API_KEY)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
 })
--- server/api/meetup.js
+++ server/api/meetup.js
@@ -1,27 +1,28 @@
 const router = require('express').Router()
 const meetups = require('meetup-api');
-const request = require('request');
+const axios = require('axios');
 
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  request({
-    url: 'http://api.meetup.com/find/upcoming_events',
-    key: process.env.MEETUP_API_KEY,
-    json: true,
-    lat: 41.8781,
-    lon: 87.6298,
-  }, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    res.json(body)
-  });
+  axios.get('http://api.meetup.com/find/upcoming_events?' +
+    'app_key=' + process.env.MEETUP_API_KEY +
+    '&lat=' + 41.8781 +
+    '&lon=' + 87.6298)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
 })
 
 router.get('/categories', (req, res, next) => {
-  request({url: 'http://api.meetup.com/find/topic_categories', json: true}, (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    res.json(body)
-  });
+  axios.get('http://api.meetup.com/find/topic_categories')
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
 })
